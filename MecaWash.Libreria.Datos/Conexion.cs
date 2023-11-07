a4e945cec07deba7e3e1cd9302cfa913e23c670a
VikusS3 <felix21soc@gmail.com>
2023-11-07 14:55:33 -0500

    conexion

---
+++ MecaWash.Libreria.Datos/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MecaWash.Libreria.Datos
+{
+    public class Conexion
+    {
+        public static string cn = ConfigurationManager.ConnectionStrings["cadena"].ToString();
+    }
+}
