9c765d7007d3b5d62d8520772708a84b9034dbb5
angela <angela.damiane@istpargentina.edu.pe>
2023-11-22 23:33:46 -0500

    reportes 50%

---
+++ MecaWash.Proyecto.Presentacion/Colaborador/Administrador/reporte.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master/colaborador.Master" AutoEventWireup="true" CodeBehind="reporte.aspx.cs" Inherits="MecaWash.Proyecto.Presentacion.Colaborador.Administrador.reporte" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class = "container" >
+        <asp:Button ID="BtnCargar" runat="server" Text="Reporte Cliente " OnClick="BtnCargar_Click" />
+        <rsweb:ReportViewer CssClass="w-100" ID="ReportViewer1" runat="server" Width="845px">
+        </rsweb:ReportViewer>
+    </div>
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+</asp:Content>
+
---
+++ MecaWash.Proyecto.Presentacion/Colaborador/Administrador/reporte.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MecaWash.Libreria.Entidad;
+using MecaWash.Libreria.Negocio;
+using Microsoft.Reporting.WebForms;
+using System.IO;
+
+namespace MecaWash.Proyecto.Presentacion.Colaborador.Administrador
+{
+    public partial class reporte : System.Web.UI.Page
+    {
+        NCliente objncliente = new NCliente();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void BtnCargar_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = objncliente.ListarClientes();
+            ReportViewer1.LocalReport.DataSources.Clear();
+            ReportDataSource rdc = new ReportDataSource("DataSet1", dt);
+            ReportViewer1.LocalReport.ReportPath = Server.MapPath("../../reportes/Rclientes.rdlc");
+            ReportViewer1.LocalReport.DataSources.Add(rdc);
+            ReportViewer1.LocalReport.Refresh();
+        }
+    }
+}
